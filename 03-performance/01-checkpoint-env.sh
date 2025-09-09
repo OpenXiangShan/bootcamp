47766b335175375869239529aadfafb5fe5aaeee
ngc7331 <ngc7331@outlook.com>
2025-09-09 18:37:34 +0800

    feat: re-numbering

--- 03-performance/01-checkpoint-env.sh
+++ 03-performance/01-checkpoint-env.sh
@@ -1,7 +1,6 @@
 # This script will setup XiangShan environment variables for checkpoint
-# See 07-checkpoint.ipynb
+# See 01-checkpoint.ipynb
 
-echo === xs-env ===
 cd .. && source env.sh && cd -
 
 # constant
@@ -9,7 +8,7 @@ export WORKLOAD=stream_100000
 export CHECKPOINT_INTERVAL=$((50*1000*1000))
 
 # path
-export SIMPOINT_HOME=$TUTORIAL_HOME/07-checkpoint
+export SIMPOINT_HOME=$WORK_DIR/checkpoint
 
 export PAYLOAD_PATH=$READY2RUN_HOME/07-checkpoint
 
@@ -23,19 +22,6 @@ export PROFILING_RESULT_PATH=$RESULT_PATH/profiling
 export NEMU=$NEMU_HOME/build/riscv64-nemu-interpreter
 export GCPT=$GCPT_PATH/build/gcpt.bin
 export SIMPOINT=$NEMU_HOME/resource/simpoint/simpoint_repo/bin/simpoint
-
-echo === checkpoint-env ===
-echo SET WORKLOAD: ${WORKLOAD}
-echo SET CHECKPOINT_INTERVAL: ${CHECKPOINT_INTERVAL}
-echo SET SIMPOINT_HOME: ${SIMPOINT_HOME}
-echo SET PAYLOAD_PATH: ${PAYLOAD_PATH}
-echo SET GCPT_PATH: ${GCPT_PATH}
-echo SET LOG_PATH: ${LOG_PATH}
-echo SET RESULT_PATH: ${RESULT_PATH}
-echo SET PROFILING_RESULT_PATH: ${PROFILING_RESULT_PATH}
-echo SET NEMU: ${NEMU}
-echo SET GCPT: ${GCPT}
-echo SET SIMPOINT: ${SIMPOINT}
 
 # pre-check
 
--- env.sh
+++ env.sh
@@ -1,10 +1,15 @@
 export XS_PROJECT_ROOT=$(pwd)
-export NEMU_HOME=$(pwd)/NEMU
-export AM_HOME=$(pwd)/nexus-am
-export NOOP_HOME=$(pwd)/XiangShan
-export DRAMSIM3_HOME=$(pwd)/DRAMsim3
-export GEM5_HOME=$(pwd)/GEM5
-export ASSETS_DIR=$(pwd)/assets
+
+# submodule
+export NEMU_HOME=${XS_PROJECT_ROOT}/NEMU
+export AM_HOME=${XS_PROJECT_ROOT}/nexus-am
+export NOOP_HOME=${XS_PROJECT_ROOT}/XiangShan
+export DRAMSIM3_HOME=${XS_PROJECT_ROOT}/DRAMsim3
+export GEM5_HOME=${XS_PROJECT_ROOT}/GEM5
+
+# assets and workdir
+export ASSETS_DIR=${XS_PROJECT_ROOT}/assets
+export WORK_DIR=${XS_PROJECT_ROOT}/work
 
 # migrated from tutorial@rvsc'25, need update
 export READY2RUN_HOME=${ASSETS_DIR}/ready-to-run
